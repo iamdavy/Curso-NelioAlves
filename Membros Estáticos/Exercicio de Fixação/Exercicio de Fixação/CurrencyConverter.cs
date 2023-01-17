f715a39150e95caf0d20f7570fbaeefa29c5fc3e
iamdavy <106548054+iamdavy@users.noreply.github.com>
2023-01-17 18:04:21 -0300

    Updates C#

---
+++ Membros Estáticos/Exercicio de Fixação/Exercicio de Fixação/CurrencyConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_de_Fixação {
+    internal class CurrencyConverter {
+
+      public static double Iof = 6.0;
+
+      public static double DollarToReal(double dollar, double amount) {
+            
+            double total = dollar * amount;
+
+            return total + total * Iof / 100.0;
+        }
+
+    }
+}
---
+++ Membros Estáticos/Exercicio de Fixação/Exercicio de Fixação/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio_de_Fixação {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dõlares você vai comprar? ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = CurrencyConverter.DollarToReal(value, price);
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
