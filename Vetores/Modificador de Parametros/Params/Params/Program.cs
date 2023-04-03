a111ee3e70a806e644acaf5349c5ddb07099fad2
Davy Silva <davy.1359@gmail.com>
2023-04-03 14:32:17 -0300

    Lista update

---
+++ Vetores/Modificador de Parametros/Params/Params/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 3, 4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
